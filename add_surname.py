c2b34e82f3da93dbaf7b4d875b967669cfd2a2f8
PringleElUno <pringlel@oregonstate.edu>
2024-11-03 14:47:37 -0800

    Assignment 6b

---
+++ add_surname.py
@@ -0,0 +1,9 @@
+# Author: Luis Pringle
+# GitHub username: PringleElUno
+# Date: 11/03/2024
+
+# Define a function named add_surname that takes as a parameter a list of first names
+
+def add_surname(names):
+    # Use of list comprehension to filer names that start with "K" and "Kardashian"
+    return [name + "Kardashian" for name in names if name.startswith (" K ")]
