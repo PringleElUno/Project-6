f7d3942b1e432706993907f64e6071e326a517f3
PringleElUno <pringlel@oregonstate.edu>
2024-11-03 17:16:02 -0800

    Assignment 6b

--- add_surname.py
+++ add_surname.py
@@ -6,4 +6,6 @@
 
 def add_surname(names):
     # Use of list comprehension to filer names that start with "K" and "Kardashian"
-    return [name + "Kardashian" for name in names if name.startswith (" K ")]
+    return [name + " Kardashian " for name in names if name.startswith ("K")]
+
+print(add_surname(["Kiki", "Krystal", "Pavel", "MaryKay", "Annie", "Koala"]))
